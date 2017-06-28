16ba93f570f0743bc085e7e43c5095382705a4b6
Michael Mendis <mmendis@partners.org>
2017-06-28 15:52:34 -0400

    Added documentation to the code

--- edu.harvard.i2b2.crc/src/server/edu/harvard/i2b2/crc/dao/setfinder/QueryResultPatientSQLCountGenerator.java
+++ edu.harvard.i2b2.crc/src/server/edu/harvard/i2b2/crc/dao/setfinder/QueryResultPatientSQLCountGenerator.java
@@ -1,5 +1,31 @@
 package edu.harvard.i2b2.crc.dao.setfinder;
 
+
+/**
+ * To add Length of Stay (This is for Oracle and Postgresl)  For Sql Server change the sql statement from (DX to #DX)
+ * 
+ * Add a entry to QT_BREAKDOWN_PATH
+ *     NAME = LENGTH_OF_STAY
+ *     VALUE = select length_of_stay as patient_range, count(distinct a.PATIENT_num) as patient_count  from visit_dimension a, DX b where a.patient_num = b.patient_num group by a.length_of_stay order by 1
+ * 
+ * Add a entry to QT_QUERY_RESULT_TYPE
+ *     RESULT_TYPE_ID = 13 (Or any unused number)
+ *     NAME = LENGTH_OF_STAY
+ *     DESCRIPTION = Length of Dtay Brealdown
+ *     DISPLAY_TYPE_ID = CATNUM
+ *     VISUAL_ATTRIBUTE_TYPE_ID = LA
+ *         
+ * Add a new <entry> in CRCApplicationContext.xml
+ * 	<entry>
+ *           <key>
+ *             <value>LENGTH_OF_STAY</value>
+ *           </key>
+ *           <value>edu.harvard.i2b2.crc.dao.setfinder.QueryResultPatientSQLCountGenerator</value>
+ *         </entry>
+ * 
+*/
+
+
 import java.io.StringWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
