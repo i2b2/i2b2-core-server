572e155c96fb55f50951825c7972f342af4f6e15
Mickaël Misbach <mickael@thehyve.nl>
2018-08-05 23:22:42 +0200

    add copyright and license

--- edu.harvard.i2b2.pm/src/edu/harvard/i2b2/pm/util/SecurityAuthenticationOIDC.java
+++ edu.harvard.i2b2.pm/src/edu/harvard/i2b2/pm/util/SecurityAuthenticationOIDC.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2017 - 2018  The Hyve B.V.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package edu.harvard.i2b2.pm.util;
 
 import com.auth0.jwk.*;
