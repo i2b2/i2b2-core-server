7804a82d63b54318b30b8db641aea2b41cb2bb55
mem61 <mem61@mikes-mbp-3.partners.org>
2018-10-17 14:42:56 -0400

    Removed old i2b2 license

--- edu.harvard.i2b2.fr/src/test/edu/harvard/i2b2/fr/ws/PostFiletoFRC_SOAPTest.java
+++ edu.harvard.i2b2.fr/src/test/edu/harvard/i2b2/fr/ws/PostFiletoFRC_SOAPTest.java
@@ -7,10 +7,7 @@
  * the terms of the Healthcare Disclaimer.
  ******************************************************************************/
 /*
- * Copyright (c) 2006-2012 Massachusetts General Hospital
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the i2b2 Software License v2.1
- * which accompanies this distribution.
+
  *
  * Contributors:
  *     Bill Wang
--- edu.harvard.i2b2.workplace/src/edu/harvard/i2b2/workplace/util/WorkplaceUtil.java
+++ edu.harvard.i2b2.workplace/src/edu/harvard/i2b2/workplace/util/WorkplaceUtil.java
@@ -7,10 +7,7 @@
  * the terms of the Healthcare Disclaimer.
  ******************************************************************************/
 /*
- * Copyright (c) 2006-2007 Massachusetts General Hospital
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the i2b2 Software License v1.0
- * which accompanies this distribution.
+
  *
  * Contributors:
  *                 Raj Kuttan
